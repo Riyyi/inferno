225dff18c72b8e86a3304516639f325d64f7c74d
Riyyi <riyyi3@gmail.com>
2021-01-29 01:15:57 +0100

    Align glm vector elements when printing

--- inferno/src/inferno/component/transformcomponent.cpp
+++ inferno/src/inferno/component/transformcomponent.cpp
@@ -4,17 +4,29 @@ namespace Inferno {
 
 	const LogStream& operator<<(const LogStream& stream, const glm::vec2& value)
 	{
-		return stream << "{ " << value.x << ", " << value.y << " }";
+		return stream << "{ "
+		              << (value.x >= 0 ? " " : "") << value.x << ", "
+		              << (value.y >= 0 ? " " : "") << value.y
+		              << " }";
 	}
 
 	const LogStream& operator<<(const LogStream& stream, const glm::vec3& value)
 	{
-		return stream << "{ " << value.x << ", " << value.y << ", " << value.z << " }";
+		return stream << "{ "
+		              << (value.x >= 0 ? " " : "") << value.x << ", "
+		              << (value.y >= 0 ? " " : "") << value.y << ", "
+		              << (value.z >= 0 ? " " : "") << value.z
+		              << " }";
 	}
 
 	const LogStream& operator<<(const LogStream& stream, const glm::vec4& value)
 	{
-		return stream << "{ " << value.x << ", " << value.y << ", " << value.z << ", " << value.w << " }";
+		return stream << "{ "
+		              << (value.x >= 0 ? " " : "") << value.x << ", "
+		              << (value.y >= 0 ? " " : "") << value.y << ", "
+		              << (value.z >= 0 ? " " : "") << value.z << ", "
+		              << (value.w >= 0 ? " " : "") << value.w
+		              << " }";
 	}
 
 	const LogStream& operator<<(const LogStream& stream, const glm::mat4& value)
