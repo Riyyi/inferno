81d6f062388abe129b84f301bad8ef087dae7780
Riyyi <riyyi3@gmail.com>
2021-01-29 01:20:55 +0100

    Float comparison should be between two floats

--- inferno/src/inferno/component/transformcomponent.cpp
+++ inferno/src/inferno/component/transformcomponent.cpp
@@ -5,27 +5,27 @@ namespace Inferno {
 	const LogStream& operator<<(const LogStream& stream, const glm::vec2& value)
 	{
 		return stream << "{ "
-		              << (value.x >= 0 ? " " : "") << value.x << ", "
-		              << (value.y >= 0 ? " " : "") << value.y
+		              << (value.x >= 0.0f ? " " : "") << value.x << ", "
+		              << (value.y >= 0.0f ? " " : "") << value.y
 		              << " }";
 	}
 
 	const LogStream& operator<<(const LogStream& stream, const glm::vec3& value)
 	{
 		return stream << "{ "
-		              << (value.x >= 0 ? " " : "") << value.x << ", "
-		              << (value.y >= 0 ? " " : "") << value.y << ", "
-		              << (value.z >= 0 ? " " : "") << value.z
+		              << (value.x >= 0.0f ? " " : "") << value.x << ", "
+		              << (value.y >= 0.0f ? " " : "") << value.y << ", "
+		              << (value.z >= 0.0f ? " " : "") << value.z
 		              << " }";
 	}
 
 	const LogStream& operator<<(const LogStream& stream, const glm::vec4& value)
 	{
 		return stream << "{ "
-		              << (value.x >= 0 ? " " : "") << value.x << ", "
-		              << (value.y >= 0 ? " " : "") << value.y << ", "
-		              << (value.z >= 0 ? " " : "") << value.z << ", "
-		              << (value.w >= 0 ? " " : "") << value.w
+		              << (value.x >= 0.0f ? " " : "") << value.x << ", "
+		              << (value.y >= 0.0f ? " " : "") << value.y << ", "
+		              << (value.z >= 0.0f ? " " : "") << value.z << ", "
+		              << (value.w >= 0.0f ? " " : "") << value.w
 		              << " }";
 	}
 
